c1c22b4d757f6dc78abb351f979beda5ff65d821
Taurin190 <taura.koichi@gmail.com>
2019-02-07 17:06:04 +0900

    add setup.py from template

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(
+    name='py-db-migration',
+    version='0.0.1',
+    description='Python CLI tool to migrate from old database to new database',
+    long_description=README.md,
+    author='Koichi Taura',
+    author_email='taura.koichi@gmail.com',
+    url='https://github.com/Taurin190/py-db-migration',
+    license=license,
+    packages=find_packages()
+)
