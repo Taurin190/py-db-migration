f68b35c1dfe26292ac286cedcc7c891c723ecdb8
Taurin190 <taura.koichi@gmail.com>
2019-02-14 17:44:23 +0900

    add exec files

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ setup(
     name='py-db-migration',
     version='0.0.1',
     description='Python CLI tool to migrate from old database to new database',
-    long_description=README.md,
+    long_description=readme,
     author='Koichi Taura',
     author_email='taura.koichi@gmail.com',
     url='https://github.com/Taurin190/py-db-migration',
     license=license,
-    packages=find_packages()
+    packages=[''],
+    install_requires=[
+        'configparser',
+        'pymongo',
+        'MySQLdb'
+    ]
 )
---
+++ src/bin/migrate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from src.utils.conf_util import get_conf
+from src.db.mysql import MySQL
+
+
+
